59b7cc74a751235bcfd6f7c6c56fe9ef6a5395f0
Manuel Westermeier <150041048+ManuelWestermeier@users.noreply.github.com>
2024-04-12 21:13:11 +0200

    Add files via upload

--- boot.asm
+++ boot.asm
@@ -20,6 +20,7 @@ step2 :
 	; print hello world
 	mov si, message
 	call print
+	jmp screen
 	jmp $
 
 print :
@@ -37,6 +38,43 @@ print_char :
 	mov ah, 0eh
 	int 0x10
 	ret
+screen :
+    mov ax,13h
+    int 10h
+    ; draw palette in 32x8 squares, each square 5x5 pixels big (so 160x40px)
+    push 0a000h
+    pop es
+    xor di,di
+    xor ax,ax  ; color
+    mov cx,8   ; big rows (each having 32 5x5 squares)
+bigRowLoop:
+    mov bx,10 ; pixel height of single row
+rowLoop:
+    mov dx,32 ; squares per row
+    push ax
+    push di
+squareLoop:
+    ; draw 10 pixels with "ah:al" color, ++color, di += 5
+    mov [es:di],ax
+    mov [es:di+2],ax
+    mov [es:di+4],al
+    add ax,0101h
+    add di,10
+    dec dx
+    jnz squareLoop
+    pop di
+    pop ax     ; restore color for first square
+    add di,320 ; move di to start of next line
+    dec bx     ; do next single pixel line
+    jnz rowLoop
+    ; one row of color squares is drawn, now next 32 colors
+    add ax,02020h ; color += 32
+    dec cx
+    jnz bigRowLoop
+    ; wait for any key and exit
+    xor ah,ah
+    int 16h
+    ret
 
 message: db '/Manuel/Westermeier/OS/V1.0/', 0
 times 510-($ - $$) db 0
